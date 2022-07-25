339913fdc2185a58111b28d3bb6c36698c830289
ZimnyCat <thezimnycat@gmail.com>
2022-07-25 11:59:47 +0500

    rewrite HoleTrap

--- src/main/java/zimnycat/reznya/utils/HoleTrap.java
+++ src/main/java/zimnycat/reznya/utils/HoleTrap.java
@@ -10,33 +10,68 @@ import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Direction;
 import net.minecraft.util.math.Vec3d;
 import zimnycat.reznya.base.UtilBase;
+import zimnycat.reznya.base.settings.SettingNum;
 import zimnycat.reznya.events.TickEvent;
+import zimnycat.reznya.libs.Delay;
 import zimnycat.reznya.libs.Finder;
-import zimnycat.reznya.libs.WorldLib;
+
+import java.util.Arrays;
+import java.util.List;
 
 public class HoleTrap extends UtilBase {
-    public HoleTrap() { super("HoleTrap", "Traps players in holes"); }
+    Delay delay = new Delay();
+
+    public HoleTrap() {
+        super(
+                "HoleTrap", "Traps players in holes",
+                new SettingNum("delay", 200, 1, 1000)
+        );
+    }
 
     @Subscribe
     public void onTick(TickEvent e) {
+        delay.setDelay((long) setting("delay").num().value);
+        if (!delay.check()) return;
+
         for (PlayerEntity player : mc.world.getPlayers()) {
-            if (mc.player.distanceTo(player) >= 5 || player == mc.player) continue;
+            if (mc.player.distanceTo(player) >= 7 || player == mc.player
+                    || player.getY() != Math.floor(player.getY())
+                    || (player.getX() - Math.floor(player.getX())) > 0.7
+                    || (player.getX() - Math.floor(player.getX())) < 0.3
+                    || (player.getZ() - Math.floor(player.getZ())) > 0.7
+                    || (player.getZ() - Math.floor(player.getZ())) < 0.3) continue;
 
-            BlockPos obsidian = player.getBlockPos().up(2);
-            if (!mc.world.getBlockState(obsidian).getBlock().equals(Blocks.AIR) || !WorldLib.isHole(player.getBlockPos())) continue;
+            BlockPos playerPos = player.getBlockPos();
 
-            Integer slot = Finder.find(Items.OBSIDIAN, true);
-            if (slot == null) return;
-
-            int pre = mc.player.getInventory().selectedSlot;
-            mc.player.getInventory().selectedSlot = slot;
-
-            mc.interactionManager.interactBlock(mc.player, Hand.MAIN_HAND,
-                    new BlockHitResult(new Vec3d(obsidian.getX(), obsidian.getY(), obsidian.getZ()), Direction.UP, obsidian, true)
+            List<BlockPos> poses = Arrays.asList(
+                    playerPos.north(),
+                    playerPos.east(),
+                    playerPos.south(),
+                    playerPos.west(),
+                    playerPos.down(),
+                    playerPos.up(2)
             );
-            mc.player.swingHand(Hand.MAIN_HAND);
 
-            mc.player.getInventory().selectedSlot = pre;
+            for (BlockPos pos : poses) {
+                if (mc.world.getBlockState(pos).getBlock().equals(Blocks.OBSIDIAN)
+                        || mc.world.getBlockState(pos).getBlock().equals(Blocks.BEDROCK)) continue;
+
+                if (mc.world.getBlockState(pos).isAir() && Math.sqrt(pos.getSquaredDistance(mc.player.getPos())) < 5) {
+                    Integer slot = Finder.find(Items.OBSIDIAN, true);
+                    if (slot == null) return;
+
+                    int pre = mc.player.getInventory().selectedSlot;
+                    mc.player.getInventory().selectedSlot = slot;
+
+                    mc.interactionManager.interactBlock(mc.player, Hand.MAIN_HAND,
+                            new BlockHitResult(new Vec3d(pos.getX(), pos.getY(), pos.getZ()), Direction.UP, pos, true)
+                    );
+                    mc.player.swingHand(Hand.MAIN_HAND);
+
+                    mc.player.getInventory().selectedSlot = pre;
+                    return;
+                }
+            }
         }
     }
 }
