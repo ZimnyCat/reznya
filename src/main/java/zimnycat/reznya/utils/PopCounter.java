b8c050138c98b1a4992fbb13c59b7e2a85b081a9
ZimnyCat <thezimnycat@gmail.com>
2022-10-04 11:02:04 +0500

    add some options to PopCounter

--- src/main/java/zimnycat/reznya/utils/PopCounter.java
+++ src/main/java/zimnycat/reznya/utils/PopCounter.java
@@ -5,6 +5,7 @@ import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.PlayerEntity;
 import zimnycat.reznya.Utilrun;
 import zimnycat.reznya.base.UtilBase;
+import zimnycat.reznya.base.settings.SettingBool;
 import zimnycat.reznya.events.EntityStatusEvent;
 
 import java.util.HashMap;
@@ -13,13 +14,17 @@ public class PopCounter extends UtilBase {
     HashMap<String, Integer> pops = new HashMap<>();
 
     public PopCounter() {
-        super("PopCounter", "Counts totem pops");
+        super(
+                "PopCounter", "Counts totem pops",
+                new SettingBool("resetOnDeath", true),
+                new SettingBool("countOwn", true)
+        );
     }
 
     @Subscribe
     public void pop(EntityStatusEvent event) {
         Entity entity = event.getEntity();
-        if (!(entity instanceof PlayerEntity)) return;
+        if (!(entity instanceof PlayerEntity) || (!setting("countOwn").bool().value && entity.equals(mc.player))) return;
 
         String name = entity.getDisplayName().getString();
 
@@ -32,7 +37,7 @@ public class PopCounter extends UtilBase {
                 pops.put(name, 1);
                 clientMessage(Utilrun.highlight(name) + " popped " + Utilrun.highlight("1") + " totem");
             }
-        } else if (event.getStatus() == 3 && pops.containsKey(name)) {
+        } else if (event.getStatus() == 3 && setting("resetOnDeath").bool().value && pops.containsKey(name)) {
             clientMessage(Utilrun.highlight(name) + " died after popping " + Utilrun.highlight(String.valueOf(pops.get(name))) + " totems");
             pops.remove(name);
             clientMessage("Pop counter reset for " + Utilrun.highlight(name));
